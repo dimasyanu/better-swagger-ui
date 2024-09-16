50c384bcb2b3323b59f2a04ba8149d4e72db4db8
Dimas Yanu <dimasyanu15@gmail.com>
2024-09-16 18:34:08 +0700

    Separate between nav item and sub menu toggler

--- src/components/NavigationDrawer.vue
+++ src/components/NavigationDrawer.vue
@@ -17,83 +17,48 @@ onMounted(async () => {
   await api
     .getJson()
     .then((res) => globalStore.storeData(res))
-    .catch((err) => {})
-    .finally(() => {})
+    .catch((err) => { })
+    .finally(() => { })
 })
 </script>
 
 <template>
   <v-navigation-drawer>
     <v-list class="pb-5">
-      <v-list-item class="pt-3 pb-2">
-        <v-text-field
-          id="group-search-input"
-          :loading="false"
-          class="d-sm-inline-block w-100"
-          append-inner-icon="mdi-magnify"
-          density="compact"
-          label="Search"
-          placeholder="Search"
-          variant="outlined"
-          hide-details
-          single-line
-          v-model="store.tagSearch"
-        ></v-text-field>
+      <v-list-item class="pb-2">
+        <v-text-field id="group-search-input" :loading="false" class="d-sm-inline-block w-100"
+          append-inner-icon="mdi-magnify" density="compact" label="Search" placeholder="Search" variant="outlined"
+          hide-details single-line v-model="store.tagSearch"></v-text-field>
       </v-list-item>
 
-      <v-divider class="py-2"></v-divider>
+      <v-divider></v-divider>
 
-      <v-list-item
-        v-if="store.filteredTags.length < 1"
-        class="text-grey text-center"
-      >
+      <v-list-item v-if="store.filteredTags.length < 1" class="text-grey text-center">
         No endpoints
       </v-list-item>
-      <v-list-item
-        link
-        v-for="(tag, i) of store.filteredTags"
-        :key="i"
-        append-icon="mdi-chevron-right"
-      >
+      <v-list-item link v-for="(tag, i) of store.filteredTags" :key="i">
         {{ tag }}
-        <v-menu
-          :open-on-focus="false"
-          activator="parent"
-          open-delay="50"
-          close-delay="50"
-          open-on-hover
-          submenu
-        >
-          <v-list class="py-0">
-            <v-list-item
-              link
-              v-for="(endpoint, j) in globalStore.apiData.find(x => x.tag === tag)!.endpoints"
-              :key="j"
-            >
-              <v-chip
-                class="method mr-3 my-2 text-uppercase"
-                label
-                size="small"
-                variant="flat"
-                :color="
-                  getColorForMethod(endpoint.method, globalStore.isDarkMode)
-                "
-              >
-                {{ endpoint.method }}
-              </v-chip>
-              <v-list-item-title class="inline">
-                {{ endpoint.path }}
-              </v-list-item-title>
-              <v-tooltip
-                v-if="!isNullOrEmpty(endpoint.request.summary)"
-                activator="parent"
-                location="end"
-              >
-                {{ endpoint.request.summary }}
-              </v-tooltip>
-            </v-list-item>
-          </v-list>
-        </v-menu>
+        <div class="submenu-toggle inline-block absolute h-full t-0 r-0 px-3 d-flex align-center cursor-default"
+          :class="{ 'dark': globalStore.isDarkMode }">
+          <v-icon icon="mdi-chevron-right"></v-icon>
+          <v-menu :open-on-focus="false" activator="parent" open-delay="50" close-delay="50" open-on-hover submenu>
+            <v-list class="py-0">
+              <v-list-item link v-for="(endpoint, j) in globalStore.apiData.find(x => x.tag === tag)!.endpoints"
+                :key="j">
+                <v-chip class="method mr-3 my-2 text-uppercase" label size="small" variant="flat"
+                  :color="getColorForMethod(endpoint.method, globalStore.isDarkMode)">
+                  {{ endpoint.method }}
+                </v-chip>
+                <v-list-item-title class="inline">
+                  {{ endpoint.path }}
+                </v-list-item-title>
+                <v-tooltip v-if="!isNullOrEmpty(endpoint.request.summary)" activator="parent" location="end">
+                  {{ endpoint.request.summary }}
+                </v-tooltip>
+              </v-list-item>
+            </v-list>
+          </v-menu>
+        </div>
       </v-list-item>
     </v-list>
   </v-navigation-drawer>
